15df0cc8a0f3fb76398c8940afd22577616c66ec
not-aoyama <glitterinformer@gmail.com>
2025-07-06 14:38:27 -0400

    Clicking the login button now causes the client to attempt to connect to the server.

--- assets/login.js
+++ assets/login.js
@@ -1,13 +1,33 @@
+import {
+    Client
+} from "https://unpkg.com/archipelago.js/dist/archipelago.min.js";
+
+var client;
+
 $(document).ready(function () {
+    // Connect to the server when the login button is clicked.
     $("#login-submit").click(function () {
-        connectionInfo = {
+        var connectionInfo = {
             hostport: $("#host-port-input").val(),
             game: "Chests 'n' Keys",
-            name: $("#slot-name-input").val(),
+            slot: $("#slot-name-input").val(),
             password: $("#password-input").val(),
             items_handling: 0b111
         };
 
-        console.log(connectionInfo);
+        client = new Client();
+        client
+            .login(
+                connectionInfo.hostport,
+                connectionInfo.slot,
+                connectionInfo.game,
+                {password: connectionInfo.password}
+            )
+            .then(() => {
+                console.log("Chee-hoo! You're now logged in!");
+            })
+            .catch((error) => {
+                console.log("Ruh-roh, failed to connect!", error);
+            });
     });
 });
