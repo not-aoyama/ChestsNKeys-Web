9ff4380a5c10d965d2cdad849fec1ea7e5fbe6c1
not-aoyama <glitterinformer@gmail.com>
2026-07-27 21:32:11 -0400

    Added an ARIA live region to the main game screen that notifies screen reader users when a chest is unlocked or opened.

--- src/scripts/mainGame.ts
+++ src/scripts/mainGame.ts
@@ -145,6 +145,26 @@ function doesChestStartLocked(chestNumber : number) : boolean {
    return chestNumber > numberUnlockedChests;
 }
 
+/**
+ * In the main game window, there is an ARIA live region that notifies screen readers of automatic changes.
+ * This live region cannot consist of the entire list of chests; otherwise, the screen reader will be overwhelmed.
+ * There also cannot be one live region for each chest; otherwise, the screen reader will have too much overhead.
+ * Instead, the live region solely consists of some text: one or more "toast messages".
+ * This text is invisible, and can only be accessed by a screen reader.
+ * Each time a chest is unlocked or emptied, a toast message is added to announce this.
+ * After a short amount of time, each toast message should be deleted to avoid clutter.
+ * 
+ * @param screenReaderMessage the new message to display. This will completely override the old one.
+ */
+function displayToScreenReader(screenReaderMessage : string) : void {
+    let newToastMsg : HTMLParagraphElement = document.createElement("p");
+    $(newToastMsg).text(screenReaderMessage);
+    $("#locations-toast").append(newToastMsg);
+    setTimeout(() => {
+        newToastMsg.remove();
+    }, 2000);
+}
+
 // Updates the appearance and functionality of the location with the given ID to show it has been checked.
 export function displayLocationChecked(locationId : number) : void {
     var chestNumber = locationId - LOCATION_ID_PREFIX;
@@ -166,12 +186,15 @@ export function displayLocationChecked(locationId : number) : void {
     $(chestID).append(emptyChestSvg);
 
     // Play a sound to show that the chest was opened.
-    // We'll do this even for the free item, since I'm too lazy to search for another sound effect.
     // However, do NOT do this if the game is still loading, i.e. the player didn't click it.
     // We don't want the "chest open" sound to play if the chest was opened the last time the player logged in,
     // and the display is just being updated to show that.
     if (!isLoading)
         playSound(CHEST_OPEN_SOUND);
+
+    // Notify screen readers that this chest is opened, how many chests have been opened so far, 
+    // and how many total still need to be opened.
+    displayToScreenReader("Chest " + chestNumber + " opened! ");
 }
 
 // Updates the appearance and functionality of the chest with the given ID to show it has been unlocked.
@@ -196,13 +219,17 @@ export function displayChestUnlocked(chestNumber : number) : void {
     // Add the SVG icon.
     $(chestHtmlID).append(unlockedChestSvg);
 
-    // Play a "chest unlocked" sound.
+    // Play a "chest unlocked" sound 
     // That is, unless this chest is already empty. In that case, the user doesn't need to be alerted.
     // Also unless the chest started unlocked. In that case, the chest doesn't need to be unlocked because it was never 
     // locked in the first place.
     let chestIsEmpty = client.room.checkedLocations.includes(chestLocationID);
     if (doesChestStartLocked(chestNumber) && !chestIsEmpty)
         playSound(CHEST_UNLOCK_SOUND);
+
+    // Notify screen readers that the chest was unlocked, unless the chest is already empty.
+    if (!chestIsEmpty)
+        displayToScreenReader("Chest " + chestNumber + " unlocked!");
 }
 
 // Plays the sound with the given URL.
